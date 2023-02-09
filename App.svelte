40925c51b3445a26141a7a0e0220327c278324e2
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-02-09 18:35:29 +0900

    dekita

---
+++ App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { css } from "@emotion/css";
+
+  let x = 0;
+  function increment() {
+    x++;
+  }
+</script>
+
+<main>
+  <div>{x}</div>
+  <button
+    class={css`
+      color: red;
+    `}
+    on:click={increment}
+  >+</button>
+</main>
+
+<style>
+</style>
